a95ec130604a4efec86a0f26afc4f98861c22c14
Wakana Iha <e165750@ie.u-ryukyu.ac.jp>
2016-12-09 17:29:55 +0900

    commit success!!!(>p<)

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by e165750 on 2016/12/09.
+ */
+public class Main {
+    public static void main(String[] args){
+        String str;
+        str = null;
+        System.out.println(str.length());
+    }
+}
