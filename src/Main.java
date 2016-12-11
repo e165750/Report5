23be52deeaf513269269cb0a4790f8625a208d94
Wakana Iha <e165750@ie.u-ryukyu.ac.jp>
2016-12-12 00:05:24 +0900

    commit success!!!(>p<)

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,14 @@
  */
 public class Main {
     public static void main(String[] args){
-        String str;
-        str = null;
-        System.out.println(str.length());
+        try{
+            String str;
+            str = null;
+            System.out.println(str.length());
+
+        }catch(NullPointerException e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
     }
 }
